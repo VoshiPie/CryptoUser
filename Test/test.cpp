213fcbc1bc0b607daa0fd1c6506aba26d72f7cc2
unknown <siavosh.amanatbari@gmail.com>
2018-10-06 23:23:55 +0200

    added test.cpp and example.cpp

---
+++ Test/test.cpp
@@ -0,0 +1,308 @@
+#include "pch.h"
+/** Testing the public functions in crypto::User in crypto.h */
+#include "crypto.h" 
+
+TEST(createNewCredentials, validPassword)
+{
+	std::vector<uint8_t> credentialsBlob;
+	crypto::User user;
+	bool status = user.createNewCredentials(
+		"user1234", "isThisPasswordLongEnough", credentialsBlob);
+
+	EXPECT_EQ(true, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(createNewCredentials, shortPassword)
+{
+	std::vector<uint8_t> credentialsBlob;
+	crypto::User user;
+	bool status = user.createNewCredentials("user1234", "tooShortPw", credentialsBlob);
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(createNewCredentials, lowercaseTest)
+{
+	std::vector<uint8_t> credentialsBlob;
+	crypto::User user;
+	bool status = user.createNewCredentials(
+		"user1234", "password missing upper case", credentialsBlob);
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(createNewCredentials, uppercaseTest)
+{
+	std::vector<uint8_t> credentialsBlob;
+	crypto::User user;
+	bool status = user.createNewCredentials(
+		"user1234", "password missing upper case", credentialsBlob);
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(verifyCredentials, validPasswordAndUsername)
+{
+	std::vector<uint8_t> credentialsBlob;
+	bool status = false;
+
+	{
+		crypto::User user;
+		user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+	}
+
+	{
+		crypto::User user;
+		status = user.verifyCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+	}
+
+	EXPECT_EQ(true, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(verifyCredentials, invalidPassword)
+{
+	std::vector<uint8_t> credentialsBlob;
+	bool status = false;
+
+	{
+		crypto::User user;
+		user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+	}
+
+	{
+		crypto::User user;
+		status = user.verifyCredentials("user1234", "TheWrongPassword", credentialsBlob);
+	}
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(verifyCredentials, invalidUserName)
+{
+	std::vector<uint8_t> credentialsBlob;
+	bool status = false;
+
+	{
+		crypto::User user;
+		user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+	}
+
+	{
+		crypto::User user;
+		status = user.verifyCredentials("invalidUsername", "ThisPasswordIsValid", credentialsBlob);
+	}
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(encryptFile, nonEmptyFileData)
+{
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	crypto::User user;
+	user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+
+	std::string fileDataToEncrypt = "This is data that I want to encrypt";
+	bool status = user.encryptNewFileData(fileDataToEncrypt.data(),
+		fileDataToEncrypt.size(), encryptedBlob);
+
+	EXPECT_EQ(true, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(encryptFile, emptyFileData)
+{
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	crypto::User user;
+	user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+
+	std::string fileDataToEncrypt = "";
+	bool status = user.encryptNewFileData(fileDataToEncrypt.data(),
+		fileDataToEncrypt.size(), encryptedBlob);
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(decryptFile, validEncryptedBlob)
+{
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	crypto::User user;
+	user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+
+	std::string fileDataToEncrypt = "file data to encrypt";
+	user.encryptNewFileData(fileDataToEncrypt.data(),
+		fileDataToEncrypt.size(), encryptedBlob);
+
+	crypto::User::CryptoFile cryptoFile;
+	bool status = user.decryptFileData(encryptedBlob.data(), encryptedBlob.size(), cryptoFile);
+	if (true == status)
+	{
+		if (memcmp(cryptoFile.data.data(), fileDataToEncrypt.data(), cryptoFile.data.size()) != 0)
+		{
+			status = false;
+		}
+	}
+
+	EXPECT_EQ(true, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(decryptFile, corruptEncryptedBlob)
+{
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	crypto::User user;
+	user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+
+	std::string fileDataToEncrypt = "file data to encrypt";
+	user.encryptNewFileData(fileDataToEncrypt.data(),
+		fileDataToEncrypt.size(), encryptedBlob);
+
+	/** Corrupt blob data */
+	encryptedBlob.at(0x40) = encryptedBlob.at(0x40) + 1;
+
+	crypto::User::CryptoFile cryptoFile;
+	bool status = user.decryptFileData(encryptedBlob.data(), encryptedBlob.size(), cryptoFile);
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(decryptFile, corruptKeyInEncryptedBlob)
+{
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	crypto::User user;
+	user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+
+	std::string fileDataToEncrypt = "file data to encrypt";
+	user.encryptNewFileData(fileDataToEncrypt.data(),
+		fileDataToEncrypt.size(), encryptedBlob);
+
+	/** Corrupt blob key */
+	encryptedBlob.at(0x20) = encryptedBlob.at(0x20) + 1;
+
+	crypto::User::CryptoFile cryptoFile;
+	bool status = user.decryptFileData(encryptedBlob.data(), encryptedBlob.size(), cryptoFile);
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(decryptFile, tooSmallEncryptedBlob)
+{
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	crypto::User user;
+	user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+
+	std::string fileDataToEncrypt = "file data to encrypt";
+	user.encryptNewFileData(fileDataToEncrypt.data(),
+		fileDataToEncrypt.size(), encryptedBlob);
+
+	/** Reduce size of the blob */
+	encryptedBlob.resize(encryptedBlob.size() - 5);
+
+	crypto::User::CryptoFile cryptoFile;
+	bool status = user.decryptFileData(encryptedBlob.data(), encryptedBlob.size(), cryptoFile);
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(decryptFile, emptyEncryptedBlob)
+{
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	crypto::User user;
+	user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+
+	crypto::User::CryptoFile cryptoFile;
+	bool status = user.decryptFileData(encryptedBlob.data(), encryptedBlob.size(), cryptoFile);
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(decryptFile, invalidUser)
+{
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	{
+		crypto::User user;
+		user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+
+		std::string fileDataToEncrypt = "file data to encrypt";
+		user.encryptNewFileData(fileDataToEncrypt.data(),
+			fileDataToEncrypt.size(), encryptedBlob);
+	}
+
+	bool status = false;
+
+	{
+		crypto::User user;
+		user.createNewCredentials("anotherUser", "ThisPasswordIsValid", credentialsBlob);
+
+		crypto::User::CryptoFile cryptoFile;
+		status = user.decryptFileData(encryptedBlob.data(), encryptedBlob.size(), cryptoFile);
+	}
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(decryptFile, emptyCryptoFileParameters)
+{
+
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	crypto::User user;
+	user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+
+	crypto::User::CryptoFile cryptoFile;
+	std::string fileDataToEncrypt = "file data to encrypt";
+	bool status = user.encryptFileData(cryptoFile.cryptoParams, fileDataToEncrypt.data(),
+		fileDataToEncrypt.size(), encryptedBlob);
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
+
+TEST(decryptFile, invalidCryptoFileParameters)
+{
+
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	crypto::User user;
+	user.createNewCredentials("user1234", "ThisPasswordIsValid", credentialsBlob);
+
+	crypto::User::CryptoFile cryptoFile;
+	cryptoFile.cryptoParams.iv.resize(7);
+	cryptoFile.cryptoParams.key.resize(13);
+	std::string fileDataToEncrypt = "file data to encrypt";
+	bool status = user.encryptFileData(cryptoFile.cryptoParams, fileDataToEncrypt.data(),
+		fileDataToEncrypt.size(), encryptedBlob);
+
+	EXPECT_EQ(false, status);
+	EXPECT_TRUE(true);
+}
---
+++ example.cpp
@@ -0,0 +1,69 @@
+﻿
+#include "crypto.h"
+
+/**
+* Example usage of crypto.h
+*/
+
+int main()
+{
+	/** 
+	 * This data could be saved as files, you can create a credentials file (credentialBlob) for each user 
+	 * and multiple files (encryptedBlobs) for each credential file
+	 */
+	std::vector<uint8_t> credentialsBlob;
+	std::vector<uint8_t> encryptedBlob;
+
+	/** At some stage you might want to create credentials for a user, create a new instance for each user */
+	{
+		crypto::User user;
+		if (!user.createNewCredentials("user1234", "ThisPasswordisValid", credentialsBlob))
+		{
+			printf("Unable to create new credentials, \
+				password needs to be 16 characters minimum, lower and upper case!");
+			/** Handle error*/
+		}
+
+		/** A user can encrypt filedata so that it is bound to the users credentials */
+		std::string fileDataToEncrypt = "This is data that I want to encrypt";
+		
+		if (!user.encryptNewFileData(fileDataToEncrypt.data(),
+			fileDataToEncrypt.size(), encryptedBlob))
+		{
+			printf("Unable to encrypt file");
+			/** Handle error*/
+		}
+	}
+
+	/** How to verify credentials and use them when you have a credentialsBlob */
+	{
+		crypto::User user;
+		if (!user.verifyCredentials("user1234", "ThisPasswordisValid", credentialsBlob))
+		{
+			printf("Incorrect username or password");
+			/** Handle error*/
+		}
+
+		crypto::User::CryptoFile cryptoFile;
+		if (!user.decryptFileData(encryptedBlob.data(), encryptedBlob.size(), cryptoFile))
+		{
+			printf("Failed to decrypt file data");
+			/** Handle error*/
+		}
+
+		/** Change or add some data and encrypt it again */
+		std::string moreFileDataToEncrypt = " / This was added to the file data";
+		cryptoFile.data.resize(cryptoFile.data.size() + moreFileDataToEncrypt.size());
+		memcpy(cryptoFile.data.data() + cryptoFile.data.size() - moreFileDataToEncrypt.size(),
+			moreFileDataToEncrypt.data(), moreFileDataToEncrypt.size());
+
+		if (!user.encryptFileData(cryptoFile.cryptoParams,
+			cryptoFile.data.data(), cryptoFile.data.size(), encryptedBlob))
+		{
+			printf("Failed to encrypt file data with existing keys");
+			/** Handle error*/
+		}
+	}
+
+	return 0;
+}
